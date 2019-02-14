26d31511294094696ede495815f9a50f74f7918b
Aleksandar Djurovski <aleksandar.djurovski@office.it-labs.com>
2019-02-14 17:02:46 +0100

    categories in header-nav

--- src/app/components/portal/header-nav/header-nav.component.ts
+++ src/app/components/portal/header-nav/header-nav.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ICategories } from 'src/app/model/category';
-import { CategoryService } from 'src/app/services/category.service';
-import { CartService } from 'src/app/services/cart.service';
-import { ProductService } from 'src/app/services/product.service';
-import { IProduct } from 'src/app/model/iproduct';
-import { FilterService } from 'src/app/services/filter.service';
+import { Component, OnInit } from "@angular/core";
+import { ICategories } from "src/app/model/category";
+import { CategoryService } from "src/app/services/category.service";
+import { CartService } from "src/app/services/cart.service";
+import { ProductService } from "src/app/services/product.service";
+import { IProduct } from "src/app/model/iproduct";
+import { FilterService } from "src/app/services/filter.service";
+import { ProductsComponent } from "../../main/products/products.component";
 
 @Component({
-  selector: 'app-header-nav',
-  templateUrl: './header-nav.component.html',
-  styleUrls: ['./header-nav.component.scss']
+  selector: "app-header-nav",
+  templateUrl: "./header-nav.component.html",
+  styleUrls: ["./header-nav.component.scss"]
 })
 export class HeaderNavComponent implements OnInit {
   categories: ICategories;
@@ -17,6 +18,8 @@ export class HeaderNavComponent implements OnInit {
   products: IProduct;
   parentCategories: any;
   shoppingLength: any;
+  parentC: any;
+  parentCategory = [];
 
   constructor(
     private _categoriesService: CategoryService,
@@ -36,6 +39,7 @@ export class HeaderNavComponent implements OnInit {
   getProducts() {
     this._productService.getProducts().subscribe((data: IProduct) => {
       this.products = data;
+      console.log(this.products);
     });
   }
 
@@ -46,6 +50,26 @@ export class HeaderNavComponent implements OnInit {
   getCategories() {
     this._categoriesService.getCategories().subscribe((data: ICategories) => {
       this.categories = data.slice(4);
+
+      console.log(this.categories);
+      debugger;
+      for (let i = 0; i < this.categories.length; i++) {
+        if (this.categories[i].parentCategoryName) {
+          this.parentC = this.categories[i].parentCategoryName;
+          if (this.parentCategory.length === 0) {
+            this.parentCategory.push(this.parentC);
+          } else {
+            for (let x = 0; x < this.parentCategory.length; x++) {
+              if (this.parentCategory[x] !== this.parentC) {
+                this.parentCategory.push(this.parentC);
+              }
+            }
+          }
+        }
+
+      }
+      console.log("OVAAA" + this.parentCategory);
+
       this.parentCategories = data.slice(0, 4);
       this._categoriesService.categoriesList = data;
     });
@@ -56,7 +80,7 @@ export class HeaderNavComponent implements OnInit {
   }
 
   resetSearch() {
-    if (this.searchName === '') {
+    if (this.searchName === "") {
       this._filterService.getProducts(this.searchName);
     }
   }
